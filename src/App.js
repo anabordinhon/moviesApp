d3ee65d88fa09faeee0a6eeee1afc1c27e6856a9
Ana Bordinhon <anabordinhon@gmail.com>
2021-06-20 02:21:28 -0300

    uploaded files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import Movie from './components/Movie';
+import logo from './assets/movie.png'
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a6abe8330a32bdc71b77b613bf4aab9c&page=1";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=a6abe8330a32bdc71b77b613bf4aab9c&query=";
 
 function App() {
+const [movies, setMovies] = useState([]);
+const [searchTerm, setSearchTerm] = useState('');
+useEffect(()=>{
+  getMovies(FEATURED_API);
+  setSearchTerm('');
+},[]);
+
+const getMovies = (API) => {
+  fetch(API)
+    .then((res) => res.json())
+    .then((data) =>{
+      setMovies(data.results);
+  });
+};
+
+const handleOnSubmit = (e) =>{
+  e.preventDefault();
+
+  if(setSearchTerm){
+    getMovies(SEARCH_API + searchTerm);
+  }
+};
+
+const handleOnChange = (e) =>{
+  setSearchTerm(e.target.value);
+}
+
+const onClickLogo = (e) =>{
+    getMovies(FEATURED_API);
+    setSearchTerm('');
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <header>
+      <img className="logo" src = {logo} alt="logo" onClick={onClickLogo}></img>
+      <form onSubmit={handleOnSubmit}>
+      <input 
+          className ="search" 
+          type="text" 
+          placeholder="Busque seu filme favorito ..."
+          value={searchTerm}
+          onChange={handleOnChange}
+        />
+      </form>
+    </header>
+    <div className="movie-container">
+      {movies.length > 0 && 
+        movies.map((movie) => <Movie key=
+        {movie.id} {...movie}/>)}
     </div>
+
+  </>
   );
 }
 
